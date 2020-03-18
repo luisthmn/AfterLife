b28e2b9fa7f14cbec0e45c7d573ef80a7e1c7bd2
Luis Thomson <luisernestothomson@hotmail.com>
2020-03-17 20:58:43 -0700

    Updated version

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-import pygame
-import classes
-import music
-import window
+import game
+
+game.AfterLife()
 
 
-screen = window.setWindow()
-player = classes.Player()  
-stage = classes.Stage()
 
-
-#Main game loop
-running = True
-while running:                                
-   
-    for event in pygame.event.get():
-        #Game controls
-        player.movimientos(event, screen)
-        #Terminate game when window is closed
-        if event.type == pygame.QUIT:
-                running = False
-                
-    #Game Graphics     
-    stage.pintarFondo(stage.fondos, screen)
-    player.pintarJugador(player.estado, screen)  
-    player.HUD(screen)
-    pygame.display.update()
